61542d901e3100809de100d3906e3883055334b1
Valentin Popov <valentinp@4psa.me>
2024-10-15 06:40:43 +0000

    Add missing conf file, slight info message update

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-echo "\033[32mInstalling TPM\033[0m\n."
+printf "\033[32mInstalling TPM\033[0m\n.\n"
 ls  ~/.tmux/plugins/tpm > /dev/null || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-echo "\033[32mSetting catppuccin custom config\033[0m\n."
+printf "\033[32mSetting catppuccin custom config\033[0m\n.\n"
 cp catppuccin_mono_tmux.conf ~/.tmux/plugins/tmux/themes/catppuccin_mono_tmux.conf
 
-
 if [ -s ~/.tmux.conf ]; then
-    echo "\033[32mBacking up existing tmux configuration\033[0m\n."
+    printf "\033[32mBacking up existing tmux configuration\033[0m\n.\n"
     mv ~/.tmux.conf ~/.tmux.conf.bak
 else
-    echo "\033[32mNo existing tmux configuration\033[0m\n."
+    printf "\033[32mNo existing tmux configuration\033[0m\n.\n"
 fi
 
-echo "\033[32mSetting tmux configuration\033[0m\n."
+printf "\033[32mSetting tmux configuration\033[0m\n.\n"
 cp tmux_config.conf ~/.tmux.conf
-echo "For the changes to take effect, clear the tmux cache using: tmux kill-server"
+cp catppuccin_rounded_minimal.conf ~/tmux_config/catppuccin_rounded_minimal.conf
+printf "For the changes to take effect, clear the tmux cache using: tmux kill-server\n"
